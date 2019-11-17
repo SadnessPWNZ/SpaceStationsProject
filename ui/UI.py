38155233a767278c35181d286add9997f5324c6c
SadnessPWNZ <princessselestiapwnz@mail.ru>
2019-11-17 18:35:33 +0400

    Updated UI

--- ui/UI.py
+++ ui/UI.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from ui.untitled import Ui_Form
-from norad_id import NORAD
 import src.api_request as rq
+from norad_id import NORAD
 
 
 class MyForm(Ui_Form, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.update_cords()
 
         self.UpdateButton.clicked.connect(self.update_cords)
 
-        self.OpenInGooglMaps.clicked.connect(rq.open_browser_map)
+        self.OpenInGooglMaps.clicked.connect(self.open_map)
+
+        self.VisualPasses.clicked.connect(self.passes)
 
     def update_cords(self):
         station = self.comboBox.currentText()
@@ -26,6 +29,14 @@ class MyForm(Ui_Form, QMainWindow):
 
         self.latitude_label.setText(str(latitude))
         self.longitude_label.setText(str(longitude))
+
+    def passes(self):
+        passes_list = rq.visual_passes(25544, 37.953757, 58.336792, 129, 10, 300)
+        for i in range(len(passes_list)):
+            self.listWidget.insertItem(i + 1, str(passes_list[i]))
+
+    def open_map(self):
+        rq.open_browser_map(self.latitude_label.text(), self.longitude_label.text())
 
 
 app = QApplication(sys.argv)
