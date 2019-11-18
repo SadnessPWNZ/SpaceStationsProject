6e53ed29d74c4173e90d130aa47a2cc4ce1d5bd7
sadnessPWNZ_laptop <princessselestiapwnz@mail.ru>
2019-11-18 18:29:29 +0400

    Updated database_request

--- SpaceStationProject/src/database_backend.py
+++ SpaceStationProject/src/database_backend.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-DATABASE_PATH = r'C:\Users\princ\Desktop\DEV\SchoolProject\sample3.sqlite'
+DATABASE_PATH = r'sample3.sqlite'
 
 con = sqlite3.connect(DATABASE_PATH)
 
 cur = con.cursor()
 
-result = cur.execute("""SELECT "Country of Operator/Owner" FROM data""").fetchall()
+result = cur.execute("""SELECT "Country of Operator/Owner" FROM Лист1""").fetchall()
 
 result = set(result)
 print(result)
