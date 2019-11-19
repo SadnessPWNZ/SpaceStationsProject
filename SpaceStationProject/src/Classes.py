50ddc4b09c3ed617097b508753f6ecd0c399ff2d
SadnessPWNZ <princessselestiapwnz@mail.ru>
2019-11-19 15:00:18 +0400

    Created Station class that implements station info(like NORAD,owner,county owner, etc.

--- SpaceStationProject/src/Classes.py
+++ SpaceStationProject/src/Classes.py
@@ -26,22 +26,47 @@ class Pass:
         '''
         self.startAz, self.startAzCompass, self.startTime, self.maxAz, self.maxAzCompass, self.maxTime, self.endAz, \
         self.endAzCompass, self.endTime, self.mag, self.duration = startAz, startAzCompass, startTime, maxAz, \
-        maxAzCompass, maxTime, endAz, endAzCompass, endTime, mag, duration
+                                                                   maxAzCompass, maxTime, endAz, endAzCompass, endTime, mag, duration
 
         # Convert frm UNIX to utc
-        self.startTime, self.maxTime, self.endTime = datetime.utcfromtimestamp(self.startTime).strftime('%Y-%m-%d %H:%M:%S'), \
-            datetime.utcfromtimestamp(self.maxTime).strftime('%Y-%m-%d %H:%M:%S'),\
-            datetime.utcfromtimestamp(self.endTime).strftime('%Y-%m-%d %H:%M:%S')
+        # TODO Add local timezone
+        self.startTime, self.maxTime, self.endTime = datetime.utcfromtimestamp(self.startTime).strftime(
+            '%Y-%m-%d %H:%M:%S'), \
+                                                     datetime.utcfromtimestamp(self.maxTime).strftime(
+                                                         '%Y-%m-%d %H:%M:%S'), \
+                                                     datetime.utcfromtimestamp(self.endTime).strftime(
+                                                         '%Y-%m-%d %H:%M:%S')
 
     def __str__(self):
         return f'Start Azimuth:{self.startAz}\n' \
-            f'Start Azimuth on Compass:{self.startAzCompass}\n' \
-            f'Start Time:{self.startTime}\n' \
-            f'Max Azimuth:{self.maxAz}\n' \
-            f'Max Azimuth on Compass:{self.maxAzCompass}\n' \
-            f'Max Time:{self.maxTime}\n' \
-            f'End Azimuth:{self.endAz}\n' \
-            f'End Azimuth on Compass:{self.endAzCompass}\n' \
-            f'End Time:{self.endTime}\n' \
-            f'Max visual magnitude:{self.mag}\n' \
-            f'Total visible duration of this pass:{self.duration} seconds\n'
+               f'Start Azimuth on Compass:{self.startAzCompass}\n' \
+               f'Start Time:{self.startTime}\n' \
+               f'Max Azimuth:{self.maxAz}\n' \
+               f'Max Azimuth on Compass:{self.maxAzCompass}\n' \
+               f'Max Time:{self.maxTime}\n' \
+               f'End Azimuth:{self.endAz}\n' \
+               f'End Azimuth on Compass:{self.endAzCompass}\n' \
+               f'End Time:{self.endTime}\n' \
+               f'Max visual magnitude:{self.mag}\n' \
+               f'Total visible duration of this pass:{self.duration} seconds\n'
+
+
+class Station(object):
+    def __init__(self, noradId: int, name: str, countryOperator: str, operator: str, users: str, purpose: str):
+        '''
+        :param noradId: NORAD ID of station
+        :param name: Satellite and(or) Alternative names
+        :param countryOperator: Country of operator
+        :param operator: Operator or owner
+        :param users: Government or Civil or Military
+        :param purpose: For what
+        '''
+
+        self.noradId, self.name, self.countryOperator, self.operator, self.users, self.purpose = noradId, name, countryOperator, operator, users, purpose
+
+    def info(self):
+        return f'Station name: {self.name}\nNORAD id of this object: {self.noradId}\nUsers: {self.users}, {self.countryOperator}\nOperator: {self.operator}\nPurpose:{self.purpose}'
+
+    def __str__(self):
+        return f'{self.name}'
+
