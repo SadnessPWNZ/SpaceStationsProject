7c1f4d9c9808b9b76c53170500222f8934c071cc
SadnessPWNZ <princessselestiapwnz@mail.ru>
2019-11-20 21:33:46 +0400

    Updated doc-string

--- SpaceStationProject/src/api_request.py
+++ SpaceStationProject/src/api_request.py
@@ -9,10 +9,16 @@ API_KEY = 'QRGLJV-JCPRUX-7ZLNAR-48LW'
 
 def coordinates(norad_id: int, obs_lat: float, obs_lng: float, obs_alt: float, api_key=API_KEY) -> tuple:
     '''
-    :param norad_id: norad id of Station
-    :return: latitude, longitude: tuple of station coordinates
+    Get predicted visual passes for any satellite relative to a location on Earth
+    :param api_key: API_KEY to get  access to the data
+    :param norad_id: NORAD id of current object(space station)
+    :param obs_lat: Observer's latitide (decimal degrees format)
+    :param obs_lng: Observer's longitude (decimal degrees format)
+    :param obs_alt: Observer's altitude above sea level in meters
+    :param days: Number of days of prediction (max 10)
+    :param min_visibility: Minimum number of seconds the satellite should be considered optically visible during the pass to be returned as result
+    :return latitude, longitude: Tuple of Satellite coordinates
     '''
-
     print(f'NORAD id: {norad_id}')
 
     # Gets the JSON, 25544 - the ISS(МКС) coordinates
@@ -46,10 +52,8 @@ def open_browser_map(latitude: float, longitude: float) -> None:
 
 def visual_passes(norad_id: int, obs_lat: float, obs_lng: float, obs_alt: float, days: int,
                   min_visibility: int, api_key=API_KEY, ) -> list:
-    passes_list = list()
     '''
     Get predicted visual passes for any satellite relative to a location on Earth
-
     :param api_key: API_KEY to get  access to the data
     :param norad_id: NORAD id of current object(space station)
     :param obs_lat: Observer's latitide (decimal degrees format)
@@ -63,9 +67,10 @@ def visual_passes(norad_id: int, obs_lat: float, obs_lng: float, obs_alt: float,
 
     :return: list of Pass objects
     '''
+    passes_list = list()
 
-    url = f'http://www.n2yo.com/rest/v1/satellite/visualpasses/{norad_id}/{obs_lat}/{obs_lng}/{obs_alt}/{days}/{min_visibility}/&apiKey={api_key}'
 
+    url = f'http://www.n2yo.com/rest/v1/satellite/radiopasses/{norad_id}/{obs_lat}/{obs_lng}/{obs_alt}/{days}/{min_visibility}/&apiKey={api_key}'
     # Get the JSON by request
     request = requests.get(url)
     request = json.loads(request.text)
@@ -87,6 +92,7 @@ def visual_passes(norad_id: int, obs_lat: float, obs_lng: float, obs_alt: float,
                 passes_list.append(passing)
         else:
             passes_list.append(None)
+            print('NO Any passes')
 
         return passes_list
 
